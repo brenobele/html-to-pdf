7e0a9e8dde769bc2b72d6792112cf1995e01a389
Breno <breno-beletato@hotmail.com>
2023-08-11 00:03:36 -0300

    Adicionada opção de gerar pdf com um arquivo html

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 const puppeteer = require("puppeteer");
+const fs = require('fs');
 
 (async () => {
+  // Pega a opção escolhida para gerar o PDF, por padrão é url
+  let type = process.argv.slice(2)[0] || 'url';
+
   // Cria uma instância do navegador
   const navegador = await puppeteer.launch();
 
   // Cria uma nova página
   const pagina = await navegador.newPage();
 
-  // URL para gerar o PDF
-  const site_url =
-    "https://www.bannerbear.com/blog/how-to-download-images-from-a-website-using-puppeteer/";
+  if (type === 'url') {
+    // URL para gerar o PDF
+    const site_url =
+      "https://www.bannerbear.com/blog/how-to-download-images-from-a-website-using-puppeteer/";
 
-  // Abrir a URL na página atual
-  await pagina.goto(site_url, { waitUntil: "networkidle0" });
+    // Abrir a URL na página atual
+    await pagina.goto(site_url, { waitUntil: "networkidle0" });
+  } else if (type === 'file') {
+    // Local do arquivo para gerar o PDF
+    const html = fs.readFileSync('sample.html', 'utf-8');
+    await pagina.setContent(html, { waitUntil: "domcontentloaded" });
+  } else {
+    console.log(new Error(`HTML source "${type}" is unknow.`));
+    await navegador.close();
+    return;
+  }
+
 
   // Para refletir o CSS usado ao invés do formato print
   await pagina.emulateMediaType("screen");
